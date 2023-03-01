cf3eac0368dcedd1ea6df5cc126efd8af1f6cd2e
Chetan wagh <chetanwag351@gmail.com>
2023-03-01 15:46:20 +0530

    installed axios

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
 
   useEffect(()=>{
     const url = "https://api.unicheck.com/similarity/checks/076d1e299b3c45bab8f2ef3879ae996f/report/export/6298048"
-    fetch(url, {
-        method: "GET",
-        headers: {"Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI1ZGRmMjFlOTE4NDY0NzE2MDg5YyIsImp0aSI6IjMwNDM3NTAiLCJpYXQiOjE2NzY2NDE3MzAsIm5iZiI6MTY3NjY0MTczMCwiZXhwIjoxNjc5MDYwOTMwLCJzdWIiOiI5Mzg3MjUiLCJzY29wZXMiOltdfQ.fdkR4Ek__m20zgH2YsND_rPDXO1BrOeVgU8dNCKI3oNHNj5OLylVodoWoK8cSDDm8l-qMuEwvcczlAXzbIjXkX32tFBnckDbLXIQPo3G2ezyEZL57qz0_LQTj-3ikbSK99RIGNE0Wsuoxo_58AZ5nClEo10n1PksDmr4i43YQSM"}
-    })
-    .then(r => r.text())
-    .then(text => {
-        console.log('text decoded:', text);
-        // setRichTextEditor(text)
-    });
+    // fetch(url, {
+    //     method: "GET",
+    //     headers: {"Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI1ZGRmMjFlOTE4NDY0NzE2MDg5YyIsImp0aSI6IjMwNDM3NTAiLCJpYXQiOjE2NzY2NDE3MzAsIm5iZiI6MTY3NjY0MTczMCwiZXhwIjoxNjc5MDYwOTMwLCJzdWIiOiI5Mzg3MjUiLCJzY29wZXMiOltdfQ.fdkR4Ek__m20zgH2YsND_rPDXO1BrOeVgU8dNCKI3oNHNj5OLylVodoWoK8cSDDm8l-qMuEwvcczlAXzbIjXkX32tFBnckDbLXIQPo3G2ezyEZL57qz0_LQTj-3ikbSK99RIGNE0Wsuoxo_58AZ5nClEo10n1PksDmr4i43YQSM"}
+    // })
+    // .then(r => r.text())
+    // .then(text => {
+    //     console.log('text decoded:', text);
+    //     // setRichTextEditor(text)
+    // });
     // fetch(url, {
     // headers: {
     //     "Content-Type": "application/json",
@@ -22,6 +24,13 @@ function App() {
     //     Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI1ZGRmMjFlOTE4NDY0NzE2MDg5YyIsImp0aSI6IjMwNDM3NTAiLCJpYXQiOjE2NzY2NDE3MzAsIm5iZiI6MTY3NjY0MTczMCwiZXhwIjoxNjc5MDYwOTMwLCJzdWIiOiI5Mzg3MjUiLCJzY29wZXMiOltdfQ.fdkR4Ek__m20zgH2YsND_rPDXO1BrOeVgU8dNCKI3oNHNj5OLylVodoWoK8cSDDm8l-qMuEwvcczlAXzbIjXkX32tFBnckDbLXIQPo3G2ezyEZL57qz0_LQTj-3ikbSK99RIGNE0Wsuoxo_58AZ5nClEo10n1PksDmr4i43YQSM"
     // }
     // })
+
+
+    axios.get(url, {
+      headers: {
+        "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI1ZGRmMjFlOTE4NDY0NzE2MDg5YyIsImp0aSI6IjMwNDM3NTAiLCJpYXQiOjE2NzY2NDE3MzAsIm5iZiI6MTY3NjY0MTczMCwiZXhwIjoxNjc5MDYwOTMwLCJzdWIiOiI5Mzg3MjUiLCJzY29wZXMiOltdfQ.fdkR4Ek__m20zgH2YsND_rPDXO1BrOeVgU8dNCKI3oNHNj5OLylVodoWoK8cSDDm8l-qMuEwvcczlAXzbIjXkX32tFBnckDbLXIQPo3G2ezyEZL57qz0_LQTj-3ikbSK99RIGNE0Wsuoxo_58AZ5nClEo10n1PksDmr4i43YQSM"
+      }
+    }).then(({data}) => console.log(data));
 }, [])
 
   return (
